b0e0bb5e051f66cb7d7cead98589607c262985c1
huseyinbilgic <huseyin.bilgic97@gmail.com>
2022-04-20 23:51:09 +0300

    Hüseyin Bilgiç - Kategorilerin listelenmesi ve bir kategoriye tıklanıldığında kategoriye ait ürünlerin getirilmesi.

--- BusinessLayer/TBusinessLayer.cs
+++ BusinessLayer/TBusinessLayer.cs
@@ -14,8 +14,55 @@ namespace BusinessLayer
         {
             Context = new DbManavMelihEntities();
         }
+        //Hüseyin Bilgiç ekledi.
+        public List<TblCategory> GetCategories(out string OMessage)
+        {
+            List<TblCategory> Categories = new List<TblCategory>();
+            OMessage = "";
+            try
+            {
+                Categories=(from data in Context.TblCategories select data).ToList();
+            }
+            catch (Exception ex)
+            {
+                OMessage = ex.Message;
 
+            }
+            return Categories;
+        }
+        //Hüseyin Bilgiç ekledi.
+        public List<TblProduct> GetProductsByCategoryId(int CategoryId,out string OMessage)
+        {
+            List<TblProduct> Products = new List<TblProduct>();
+            OMessage = "";
+            try
+            {
+                Products = (from data in Context.TblProducts where data.CategoryId== CategoryId select data).ToList();
+            }
+            catch (Exception ex)
+            {
+                OMessage = ex.Message;
 
+            }
+            return Products;
+        }
+
+        //Hüseyin Bilgiç ekledi.
+        public TblCategory GetCategoryById(string CategoryName, out string OMessage)
+        {
+            TblCategory Category = new TblCategory();
+            OMessage = "";
+            try
+            {
+                Category = (from data in Context.TblCategories where data.CategoryName == CategoryName select data).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                OMessage = ex.Message;
+
+            }
+            return Category;
+        }
 
     }
 }
