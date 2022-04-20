7911de5feb760d31a8051dfd8cad4a5483ab0234
Celal Serdar Ergun <celalserdarergun@protonmail.com>
2022-04-21 00:09:54 +0300

    Celal Serdar - Checkcontroller olusturuldu

---
+++ WebPortal/Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPortal.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
