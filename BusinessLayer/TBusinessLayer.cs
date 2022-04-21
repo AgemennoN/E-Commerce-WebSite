275c24450e2c83b4f6bbeb6a9a48258b0aed2ee4
TayfunF <firtinatayfun08@gmail.com>
2022-04-21 15:12:44 +0300

    [YK] - BusinessLayer'a Admin sayfasi icin GetPorduct methodu eklendi. Admin Controller Eklendi. Ürün Listelemek için Index Sayfası oluşturuldu. Web Config içerisine paket güncellemesi yapıldı. AdminLayout Eklendi. Index Sayfasında DB deki ürünlerin tablosu oluşturuldu

--- BusinessLayer/TBusinessLayer.cs
+++ BusinessLayer/TBusinessLayer.cs
@@ -9,13 +9,30 @@ namespace BusinessLayer
 {
     public class TBusinessLayer
     {
+        //[EGEMEN-GOKHAN-MELIH-TAYFUN] Admin Sayfasi Urunler Listesi Kodu
         private DbManavMelihEntities Context;
         public TBusinessLayer()
         {
             Context = new DbManavMelihEntities();
         }
 
+        public List<TblProduct> GetProductList(out string OMessage)
+        {
+            List<TblProduct> ProductList = new List<TblProduct>();
+            OMessage = "";
+            try
+            {
+                ProductList = (from Data in Context.TblProducts where Data.ProductActive == true  select Data).ToList();
+            }
+            catch (Exception ex)
+            {
 
+                OMessage = ex.Message;
+            }
+
+            return ProductList;
+        }
+        //[EGEMEN-GOKHAN-MELIH-TAYFUN] Admin Sayfasi Urunler Listesi Kodu
 
     }
 }
---
+++ WebPortal/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPortal.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        //[EGEMEN-GOKHAN-MELIH-TAYFUN] Admin Sayfasi Urunler Listesi Yapildi
+        public ActionResult Index()
+        {
+            TBusinessLayer BusinessLayer = new TBusinessLayer();
+            string OMessage;
+           ViewBag.GetList = BusinessLayer.GetProductList(out OMessage);
+            return View(ViewBag);
+        }
+        //[EGEMEN-GOKHAN-MELIH-TAYFUN] Admin Sayfasi Urunler Listesi Yapildi
+    }
+}
